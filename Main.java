04b920a43eb79525846764092bbb5f57405bf71c
local <hai@voided.systems>
2026-08-17 12:40:35 -0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+import java.awt.*;
+
+public class Main {
+  public static void main(String[] args) {  
+    World world = new World(300,300);
+   
+     
+    // Turtle yertle = new Turtle(world);
+    // // Add your code here
+    // yertle.forward();
+    // yertle.turnRight();
+    // yertle.forward();
+
+  
+    // //world.setVisible(true);
+
+    // World world2 = new World(true);
+   
+     
+    // Turtle yertle2 = new Turtle(world2);
+    // // Add your code here
+    // yertle2.forward();
+    // yertle2.turnRight();
+    // yertle2.forward();
+
+  
+    //world.setVisible(true);
+  }
+}
